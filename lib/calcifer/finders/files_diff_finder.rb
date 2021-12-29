4238f1907c2bd2f27ee37362c08c10be3fecc850
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-12-29 08:36:20 -0300

    small indentation adjustments

--- lib/calcifer/finders/files_diff_finder.rb
+++ lib/calcifer/finders/files_diff_finder.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 module Calcifer
   module Finders
     class FilesDiffFinder
       attr_reader :file_list
 
       def execute
-        @file_list = git_files.split.map(&->(file_path){
+        @file_list = git_files.split.map(&lambda { |file_path|
           file = File.open(file_path)
           file if file.size.positive? && File.extname(file.path) == '.rb'
         }).compact
       end
 
-      private 
+      private
+
       def git_files
         `git ls-files --others --exclude-standard && git diff --name-only --diff-filter=MCRTXB`
       end
