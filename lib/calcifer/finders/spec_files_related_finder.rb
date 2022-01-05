a80f68231787a650493196a49d03c9b81a968448
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2022-01-05 18:22:04 -0300

    refact spec_files_related_finder

--- lib/calcifer/finders/spec_files_related_finder.rb
+++ lib/calcifer/finders/spec_files_related_finder.rb
@@ -5,25 +5,19 @@ module Calcifer
     class SpecFilesRelatedFinder
       attr_reader :related_spec_files
 
-      def initialize(modules)
-        @modules = modules
+      def initialize(related_files_path)
+        @related_files_path = related_files_path
       end
 
       def execute
         @related_spec_files = specs_to_related_files.join(' ').strip
       end
 
-      private
-
-      def related_files_path
-        module_names = @modules.join('\\|')
-        `grep -Rlw '#{module_names}' ./app`
-      end
-
       def specs_to_related_files
-        related_files_path.split.map(&lambda { |file_path|
-          file_path.gsub('./app', './spec').gsub('.rb', '_spec.rb')
-        }).select { |file_path_spec| File.file?(file_path_spec) }
+        @related_files_path.map(&lambda { |file_path|
+          spec_file_path = file_path.gsub('./app', './spec').gsub('.rb', '_spec.rb')
+          File.file?(spec_file_path) ? spec_file_path : nil
+        }).compact
       end
     end
   end
