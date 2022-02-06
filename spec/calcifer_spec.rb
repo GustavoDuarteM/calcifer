1806a557207ad8e108364da6e5b169e45ea5608c
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2022-02-06 19:00:34 -0300

    rubocop fix

--- spec/calcifer_spec.rb
+++ spec/calcifer_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe Calcifer do
-  it "has a version number" do
-    expect(Calcifer::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Calcifer do
+  it 'has a version number' do
+    expect(Calcifer::VERSION).not_to be nil
   end
 end
